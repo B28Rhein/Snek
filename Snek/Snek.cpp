bcc710f58e6a05d680bba5d92cefa9b7457a9084
Sebastian Matejak <sebastian.mat.2201@gmail.com>
2026-02-25 12:48:06 +0100

    Init Commit

---
+++ Snek/Snek.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+int main()
+{
+	float SizeX = 1024;
+	float SizeY = 960;
+	const char* title = "Snek";
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	GLFWwindow* window = glfwCreateWindow(SizeX, SizeY, title, NULL, NULL);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+}
